44c976f9f07e63f9b35027d9d4016f0b3e075f27
Abdulsalam16 <108259140+Abdulsalam16@users.noreply.github.com>
2024-01-31 16:26:43 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "hoststarcloneproject" # Replace with your actual S3 bucket name
+    bucket = "skillifytechbucket" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
